e30c4287d846fc1411c5fea8c57213f0bc172aa7
Oscar <oscar.larsonhowe@unimelb.edu.au>
2026-06-29 15:46:29 +1000

    Assembled Installomator script

--- Installomator.sh
+++ Installomator.sh
@@ -8241,6 +8241,14 @@ nextcloudtalk)
     appNewVersion="$(curl -fsIL ${downloadURL} | grep -i ^location | grep -oE '/v[0-9]+\.[0-9]+\.[0-9]+/' | cut -d '/' -f2 | sed 's/^v//')"
     expectedTeamID="NKUJUXUJ3B"
     ;;
+nitropdf)
+    name="Nitro PDF Pro"
+    type="dmg"
+    appNewVersion=$(curl -s "https://downloads.gonitro.com/macos/pro.rss" | xmllint --xpath '//rss/channel/item/title/text()' - | head -n 1 | sed 's/[^0-9.]//g')
+    downloadURL="https://downloads.gonitro.com/macos/Nitro%20PDF%20Pro_${appNewVersion}.dmg"    
+    expectedTeamID="37C4TX3D45"
+    blockingProcesses=( "Nitro PDF Pro" )
+    ;;
 nodejs)
     name="nodejs"
     type="pkg"
