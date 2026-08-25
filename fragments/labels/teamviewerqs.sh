64614c8f438a88fd38bb9855de5c42b873b0d711
Oscar <oscar.larsonhowe@unimelb.edu.au>
2026-08-25 15:34:45 +1000

    adding SYSTEMOWNER and targetDir variables

--- fragments/labels/teamviewerqs.sh
+++ fragments/labels/teamviewerqs.sh
@@ -4,4 +4,6 @@ teamviewerqs)
     downloadURL="https://download.teamviewer.com/download/TeamViewerQS.dmg"
     appNewVersion=$(getJSONValue "$(curl -fsL https://www.teamviewer.com/en/solutions/use-cases/quicksupport/ | grep .dmg |  grep -o 'data-json="[^"]*"' | sed 's/data-json="//;s/"$//' | sed 's/&quot;/"/g' )" "data[0].versionNumber")
     expectedTeamID="H7UGFBUGV6"
+    targetDir="/Applications/Utilities"
+    SYSTEMOWNER=1
     ;;
